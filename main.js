070ee0484ad7673de7d8de75ffc78b78d9a1cbd7
Isfar Tausif <isfartousif2@gmail.com>
2022-06-08 21:46:51 +0600

    Implemented Node and "Electronified" the Project.

---
+++ main.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow, screen } = require('electron')
+
+const createWindow = () => {
+    const {width, height} = screen.getPrimaryDisplay().workAreaSize
+
+    const win = new BrowserWindow({
+        width: width > 400? (width-50) : 400,
+        height: height > 400? (height-50) : 400,
+        minWidth: 400,
+        minHeight: 400,
+        autoHideMenuBar: true
+    })
+
+    win.loadFile('reader.html')
+    // win.webContents.openDevTools()
+}
+  
+app.on("ready", createWindow)
+
+app.on("window-all-closed", () => {app.quit()})
